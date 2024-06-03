a324a632d83a4be3110ac9de2c6efd90007d2642
max1lock <alex.gerberon@gmail.com>
2024-06-03 17:07:21 +0200

    fix SportsBetsRepository

--- BakaBack/BakaBack.API/Program.cs
+++ BakaBack/BakaBack.API/Program.cs
@@ -1,8 +1,12 @@
 using BakaBack.Infrastructure;
 using BakaBack.Infrastructure.Contexts;
 using BakaBack.Infrastructure.Models;
+using BakaBack.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +24,46 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+/* builder.Services.Configure<IdentityOptions>(options =>
+{ // Fonctionnel, à utiliser plus tard en dehors de la phase de développement
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredUniqueChars = 0;
+}); */
+
+/* builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddAuthorization(options => { options.FallbackPolicy = options.DefaultPolicy; });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+               builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                                     .AllowAnyHeader());
+});
+builder.Services.AddMvc(); */
+
+// Add SportsBetRepository
+builder.Services.AddScoped<SportsBetRepository>();
 
 var app = builder.Build();
 
--- BakaBack/BakaBack.API/obj/Debug/net8.0/BakaBack.API.AssemblyInfo.cs
+++ BakaBack/BakaBack.API/obj/Debug/net8.0/BakaBack.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakaBack.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5d0ef3a1cea857744b71b646e4694a43ccfeb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5f6d3e084bf4b103453db0771c8724b90085be")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakaBack.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakaBack.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
