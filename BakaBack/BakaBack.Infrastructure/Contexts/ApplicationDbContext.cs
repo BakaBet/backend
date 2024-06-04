3a067b914ca89a2b814bbbef523ad5242800796e
max1lock <alex.gerberon@gmail.com>
2024-06-04 23:54:37 +0200

    Remove DeadCode

--- BakaBack/BakaBack.Infrastructure/Contexts/ApplicationDbContext.cs
+++ BakaBack/BakaBack.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -19,15 +19,6 @@ namespace BakaBack.Infrastructure.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-           /* modelBuilder.Entity<LifeBet>()
-                .HasKey(lb => lb.Id);
-
-            modelBuilder.Entity<Proposal>()
-                .HasKey(p => new { p.UserId, p.EventId });
-
-            modelBuilder.Entity<Participation>()
-                .HasKey(p => new { p.UserId, p.Stake });*/
         }
     }
 }
